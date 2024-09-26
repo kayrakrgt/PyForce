52e6bee7093aade8f1b258b1e1127564b7541998
kayra <94867695+kayrakrgt@users.noreply.github.com>
2024-09-26 19:52:25 +0300

    Add files via upload

---
+++ flash/main.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def Merhaba():
+    return render_template("index.html")
+
+@app.route("/home.html/")
+
+def Homie():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
